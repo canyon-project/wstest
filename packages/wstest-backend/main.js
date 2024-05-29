da9ecf7eac4af7bc000b2da5e87fd3af2f4698df
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-05-29 14:49:40 +0800

    upd

--- packages/wstest-backend/main.js
+++ packages/wstest-backend/main.js
@@ -1,13 +1,26 @@
+const express = require('express');
+const http = require('http');
 const WebSocket = require('ws');
-// import WebSocket from 'ws';
-// console.log(WebSocket,'WebSocket')
-const server = new WebSocket.Server({ port: 8080 });
-console.log(global)
-server.on('connection', socket => {
+const cors = require('cors');
+const app = express();
+// 配置 CORS 中间件
+app.use(cors());
+// 配置 Express 路由
+app.get('/vi/health', (req, res) => {
+  res.send('230614');
+});
+
+// 创建 HTTP 服务器
+const server = http.createServer(app);
+
+// 将 WebSocket 服务器附加到 HTTP 服务器
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', socket => {
   console.log('Client connected');
 
   socket.on('message', message => {
-    console.log(`Received: ${message}`);
+    console.log(`Received: ${message} ${new Date().toLocaleString()}`);
   });
 
   // 定时获取数据
@@ -22,4 +35,10 @@ server.on('connection', socket => {
   });
 });
 
-console.log('WebSocket server is running on ws://localhost:8080');
+
+
+// 启动服务器
+const PORT = 8080;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- packages/wstest-ui/src/ws.js
+++ packages/wstest-ui/src/ws.js
@@ -1,29 +1,29 @@
 // 创建WebSocket连接
-const socketCoverage = new WebSocket('ws://localhost:8080');
+const socket = new WebSocket('ws://localhost:8080');
 
 // 当WebSocket连接打开时触发
-socketCoverage.onopen = () => {
+socket.onopen = () => {
   console.log('Connected to coverage WebSocket server');
 };
 
 // 当收到WebSocket消息时触发
-socketCoverage.onmessage = (event) => {
+socket.onmessage = (event) => {
   console.log('Message from server:', event.data);
   if (JSON.parse(event.data).type === 'getcoverage') {
     // 发送覆盖率数据
-
-    console.log(JSON.stringify(window.__coverage__||{}))
-    socketCoverage.send(JSON.stringify(window.__coverage__||{}));
+    socket.send(JSON.stringify((new Function('return this')()).__coverage__||{}));
   }
 };
 
 // 当WebSocket连接关闭时触发
-socketCoverage.onclose = () => {
+socket.onclose = () => {
   console.log('Disconnected from coverage WebSocket server');
 };
 
 
 // 清理WebSocket连接
-window.addEventListener('beforeunload', () => {
-  socketCoverage.close();
+(new Function('return this')()).addEventListener('beforeunload', () => {
+  socket.close();
 });
+
+
